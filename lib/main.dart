f285ba113d434feaa3a9b3d6cad9c7b7c92543f5
honeykinng345 <rehanmunir217@gmail.com>
2022-04-05 16:50:37 -0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'NameRouteNavigaion/myApps.dart';
+
+void main() => runApp(MyApp());
+
